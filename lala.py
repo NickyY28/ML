bcf46738ee3b6c05b0d5292bee7199962adcfd67
NickyY28 <nicky28bt@gmail.com>
2026-02-22 10:49:21 +0530

    first commit

---
+++ lala.py
@@ -0,0 +1,38 @@
+''' 
+types of regression:
+1. Linear regression - used for continuous values
+2. Logistic regression - used for binary values
+3. Polynomial regression - used for non-linear relationships
+4. Ridge regression - used for high-dimensional data
+5. Lasso regression - used for feature selection
+6. ElasticNet regression - used for high-dimensional data with feature selection
+
+types of classification:
+1. Logistic regression
+2. Decision tree - used for decision making
+3. Random forest - used for decision making
+4. Support vector machine - used for decision making
+5. Naive Bayes - used for decision making
+6. KNN - used for decision making
+7. XGBoost - used for decision making
+
+Decision Tree:
+For both classification and regression 
+works like a flowchart
+how does it work:
+1. it starts with the root node
+2. it splits the data into two subsets based on the best feature
+3. it repeats the process until it reaches the leaf node
+how is it useful:
+1. it can be used for both classification and regression
+2. it can be used for both binary and multi-class classification
+how is it better than other algorithms:
+1. it is simple to understand and implement
+2. it is less prone to overfitting
+3. it can handle both categorical and numerical data
+
+- underfitting: when testing data is too much and training data is too less
+- overfitting: when training data is too much and testing data is too less 
+
+
+'''
